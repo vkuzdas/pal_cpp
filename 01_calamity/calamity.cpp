5b6e9739d5b253e0d6992c68412c3482794c7337
awesome-vojta <vojtechkuzdas@gmail.com>
2022-09-30 18:39:18 +0200

    brute-1.12: final

--- 01_calamity/calamity.cpp
+++ 01_calamity/calamity.cpp
@@ -10,55 +10,35 @@
 using namespace std;
 using wd_pair = pair<int, int>;
 
-//#include <chrono>
-
-//using namespace std::chrono;
-
 int prim_mst_per_district(vector<vector<wd_pair>> &adj, vector<int> &districts, int district);
 int prim_mst(vector<vector<wd_pair>> &adj, int startV);
 void getInput(int R, vector<vector<pair<int,int>>>& adj);
 void condense_graph(vector<vector<wd_pair>> &adj, vector<int> &districts, int D);
+vector<int> assign_districts(vector<vector<pair<int, int>>> &adj, int D);
 
-vector<int> fast_assign(vector<vector<pair<int, int>>> &adj, int D);
-
-//void print_time(const char *string, std::chrono::time_point<std::chrono::system_clock> point);
 
 int main() {
-//    std::chrono::time_point<std::chrono::system_clock> start = high_resolution_clock::now();
-    int T, D, R; // V < 250k, D-towns < 2000, E < 450k
+    int T, D, R;
     cin >> T >> D >> R;
-    vector<vector<pair<int,int>>> adj(T+1); // src -> { {w1, dest1}, {w2, dest2} }
+    vector<vector<pair<int,int>>> adj(T+1); // src -> { {w1, dest1} }
     getInput(R, adj);
-//    print_time("getinput ends at: ", start);
 
-    vector<int> districts = fast_assign(adj,  D);
-//    print_time("fastassign ends at: ", start);
+    // BFS district assign
+    vector<int> districts = assign_districts(adj,  D);
 
     // do prim in D subgraphs
     int total_cost = 0;
     for (int i = 1; i <= D; ++i) {
         int cost = prim_mst_per_district(adj, districts, i);
         total_cost = total_cost + cost;
-//        setbuf(stdout, 0); cout << total_cost << " \n";
     }
-//    print_time("prim_mst_per_district ends at: ", start);
 
     // do prim in the supergraph
     condense_graph(adj, districts, D);
     total_cost = total_cost + prim_mst(adj, 1);
     cout << total_cost;
-//    print_time("total time: ", start);
     return 0;
 }
-
-//void print_time(const char *string, std::chrono::time_point<std::chrono::system_clock> point) {
-//    auto stop = high_resolution_clock::now();
-//    auto duration = duration_cast<nanoseconds>(stop - point);
-//    setbuf(stdout, 0);
-//    cout << string;
-//    cout << duration.count()*0.000000001 << " s \n";
-//}
-
 
 /**
  * Graph condensation
@@ -69,14 +49,12 @@ int main() {
 void condense_graph(vector<vector<wd_pair>> &adj, vector<int> &districts, int D) {
     // kondenzace grafu:
     // nahradit destination za district (dest nalezi distu)
-//    setbuf(stdout, 0); cout << "nahradit dest -> dist\n";
     for (size_t src = 1; src < adj.size(); ++src) {
         for (auto &nei_pair : adj[src]) {
             nei_pair.second = districts[nei_pair.second];
         }
     }
     // odstranit edges ktere vedou d1 -> d1
-//    setbuf(stdout, 0); cout << "odstranit d1 -> d1\n";
     for (size_t src = 1; src < adj.size(); ++src) {
         vector<wd_pair> replacement;
         for (wd_pair p : adj[src]) {
@@ -90,7 +68,6 @@ void condense_graph(vector<vector<wd_pair>> &adj, vector<int> &districts, int D)
         adj[src] = replacement;
     }
     // move edge from node belonging to dist to original dist node
-//    setbuf(stdout, 0); cout << "edges to original nodes\n";
     for (size_t src = adj.size()-1; src > size_t(D); --src) { // from last src to last D
         for (auto nei_pair : adj[src]) {
             int src_dist = districts[src];
@@ -100,6 +77,9 @@ void condense_graph(vector<vector<wd_pair>> &adj, vector<int> &districts, int D)
     }
 }
 
+/**
+ * Jarnik/Prim algorithm with Min-Heap running in O(m log n)
+ */
 int prim_mst(vector<vector<wd_pair>> &adj, int startV) {
     int total_cost = 0;
     size_t counter = 0;
@@ -109,7 +89,7 @@ int prim_mst(vector<vector<wd_pair>> &adj, int startV) {
 
     // traversal
     while (counter < adj.size()-1) { // size-1 bcs of non-existent id(0) node
-        wd_pair curr = heap.top();heap.pop();
+        wd_pair curr = heap.top(); heap.pop();
         if (seenBool[curr.second]) continue; // skip visited
         total_cost = total_cost + curr.first;
         counter = counter + 1; seenBool[curr.second] = true;
@@ -152,7 +132,12 @@ int prim_mst_per_district(vector<vector<wd_pair>> &adj, vector<int> &districts, 
     return total_cost;
 }
 
-vector<int> fast_assign(vector<vector<pair<int, int>>> &adj, int D) {
+/**
+ * Assigns district to every city based on city's proximity to district city
+ *      (or based on which district has lower id in case proximity is same)
+ */
+vector<int> assign_districts(vector<vector<pair<int, int>>> &adj, int D) {
+    // setup
     vector<int> districts(adj.size());
     vector<int> traversals(adj.size());
     queue<int> q;
@@ -161,14 +146,15 @@ vector<int> fast_assign(vector<vector<pair<int, int>>> &adj, int D) {
         districts[i] = i;
         q.push(i);
     }
+
+    // BFS through graph from all district nodes
+    //   assign depth (from district node) to each node
     while (!q.empty()) {
-//        cout << q.size() << "q   " << seen.size() << " s   " << adj.size() << " adj\n";
         int curr = q.front(); q.pop();
         for(auto nei : adj[curr]) {
                 if(districts[nei.second] == 0) {
                     traversals[nei.second] = traversals[curr] + 1;
                     districts[nei.second] = districts[curr];
-//                    setbuf(stdout, 0); cout << curr; cout << "    " << nei.second; cout << " " << traversals[nei.second];                    cout << "|" << districts[nei.second] <<"\n";
                     q.push(nei.second);
                 }
         }
@@ -177,6 +163,7 @@ vector<int> fast_assign(vector<vector<pair<int, int>>> &adj, int D) {
 }
 
 void getInput(int R, vector<vector<pair<int,int>>>& adj) {
+    // reads graph from input and represents it as mirrored directed graph in adj list
     for (int i = 0; i < R; ++i) {
         int src, dest, weight;
         cin >> src >> dest >> weight;
