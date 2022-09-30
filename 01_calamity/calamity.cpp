a94da115c342d1e8ab997a1167b6a6bd947bd72f
awesome-vojta <vojtechkuzdas@gmail.com>
2022-09-30 16:57:34 +0200

    brute-1.10: slow inSet(seen) operation omitted, refactoring is needed

--- 01_calamity/calamity.cpp
+++ 01_calamity/calamity.cpp
@@ -11,33 +11,55 @@
 using namespace std;
 using wd_pair = pair<int, int>;
 
+#include <chrono>
+#include <stack>
+
+using namespace std::chrono;
+
 int prim_mst_per_district(vector<vector<wd_pair>> &adj, vector<int> &districts, int district);
 int prim_mst(vector<vector<wd_pair>> &adj, int startV);
 void getInput(int R, vector<vector<pair<int,int>>>& adj);
 bool inSet(unordered_set<int> set, int v);
 void condense_graph(vector<vector<wd_pair>> &adj, vector<int> &districts, int D);
+
 vector<int> fast_assign(vector<vector<pair<int, int>>> &adj, int D);
 
+void print_time(const char *string, std::chrono::time_point<std::chrono::system_clock> point);
+
 int main() {
+    std::chrono::time_point<std::chrono::system_clock> start = high_resolution_clock::now();
     int T, D, R; // V < 250k, D-towns < 2000, E < 450k
     cin >> T >> D >> R;
     vector<vector<pair<int,int>>> adj(T+1); // src -> { {w1, dest1}, {w2, dest2} }
     getInput(R, adj);
+    print_time("getinput ends at: ", start);
 
     vector<int> districts = fast_assign(adj,  D);
+    print_time("fastassign ends at: ", start);
 
     // do prim in D subgraphs
     int total_cost = 0;
     for (int i = 1; i <= D; ++i) {
         int cost = prim_mst_per_district(adj, districts, i);
         total_cost = total_cost + cost;
+//        setbuf(stdout, 0); cout << total_cost << " \n";
     }
+    print_time("prim_mst_per_district ends at: ", start);
 
     // do prim in the supergraph
     condense_graph(adj, districts, D);
     total_cost = total_cost + prim_mst(adj, 1);
     cout << total_cost;
+    print_time("total time: ", start);
     return 0;
+}
+
+void print_time(const char *string, std::chrono::time_point<std::chrono::system_clock> point) {
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<nanoseconds>(stop - point);
+    setbuf(stdout, 0);
+    cout << string;
+    cout << duration.count()*0.000000001 << " s \n";
 }
 
 
@@ -50,13 +72,15 @@ int main() {
 void condense_graph(vector<vector<wd_pair>> &adj, vector<int> &districts, int D) {
     // kondenzace grafu:
     // nahradit destination za district (dest nalezi distu)
-    for (auto src = 1; src < adj.size(); ++src) {
+//    setbuf(stdout, 0); cout << "nahradit dest -> dist\n";
+    for (size_t src = 1; src < adj.size(); ++src) {
         for (auto &nei_pair : adj[src]) {
             nei_pair.second = districts[nei_pair.second];
         }
     }
     // odstranit edges ktere vedou d1 -> d1
-    for (auto src = 1; src < adj.size(); ++src) {
+//    setbuf(stdout, 0); cout << "odstranit d1 -> d1\n";
+    for (size_t src = 1; src < adj.size(); ++src) {
         vector<wd_pair> replacement;
         for (wd_pair p : adj[src]) {
             int dest = p.second;
@@ -69,31 +93,34 @@ void condense_graph(vector<vector<wd_pair>> &adj, vector<int> &districts, int D)
         adj[src] = replacement;
     }
     // move edge from node belonging to dist to original dist node
-    for (auto src = adj.size()-1; src > D; --src) { // from last src to last D
+//    setbuf(stdout, 0); cout << "edges to original nodes\n";
+    for (size_t src = adj.size()-1; src > size_t(D); --src) { // from last src to last D
         for (auto nei_pair : adj[src]) {
             int src_dist = districts[src];
             adj[src_dist].push_back(nei_pair);
         }
-        adj.erase(adj.begin() + src);
+        adj.erase(adj.begin() + int(src));
     }
 }
 
 int prim_mst(vector<vector<wd_pair>> &adj, int startV) {
     int total_cost = 0;
+    size_t counter = 0;
     unordered_set<int> seen;
-    priority_queue<wd_pair, vector<wd_pair>, greater<>> heap; /// MIN-heap (PQ is max heap by default)
+    vector<bool> seenBool(adj.size(), false);
+    priority_queue<wd_pair, vector<wd_pair>, greater<wd_pair>> heap; /// MIN-heap (PQ is max heap by default)
     heap.push({0, startV});
 
     // traversal
-    while (seen.size() < adj.size()-1) { // size-1 bcs of non-existent id(0) node
-        wd_pair curr = heap.top(); heap.pop();
-        if (inSet(seen, curr.second)) continue; // skip visited
+    while (counter < adj.size()-1) { // size-1 bcs of non-existent id(0) node
+        wd_pair curr = heap.top();heap.pop();
+        if (seenBool[curr.second]) continue; // skip visited
         total_cost = total_cost + curr.first;
-        seen.insert(curr.second);
+        counter = counter + 1; seenBool[curr.second] = true;
 
         // neighbors
         for (wd_pair neighPair : adj[curr.second]) {
-            if (inSet(seen, neighPair.second)) continue; // skip visited
+            if (seenBool[neighPair.second]) continue; // skip visited
             heap.push(neighPair);
         }
     }
@@ -105,23 +132,24 @@ int prim_mst(vector<vector<wd_pair>> &adj, int startV) {
  */
 int prim_mst_per_district(vector<vector<wd_pair>> &adj, vector<int> &districts, int district) {
     int total_cost = 0;
-    auto to_visit = int(count(districts.begin(), districts.end(), district));
-    unordered_set<int> seen;
-    priority_queue<wd_pair, vector<wd_pair>, greater<>> heap; /// MIN-heap (PQ is max heap by default)
+    size_t counter = 0;
+    size_t to_visit = count(districts.begin(), districts.end(), district);
+    vector<bool> seenBool(adj.size(), false);
+    priority_queue<wd_pair, vector<wd_pair>, greater<wd_pair>> heap; /// MIN-heap (PQ is max heap by default)
     heap.push({0, district});
 
     // traversal
-    while (seen.size() != to_visit) {
+    while (counter != to_visit) {
         wd_pair curr = heap.top();
         heap.pop();
-        if (inSet(seen, curr.second)) continue; // skip visited
+        if (seenBool[curr.second]) continue; // skip visited
         total_cost = total_cost + curr.first;
-        seen.insert(curr.second);
+        counter = counter + 1; seenBool[curr.second] = true;
 
         // neighbors
         for (wd_pair neighPair : adj[curr.second]) {
             if (districts[neighPair.second] != district) continue;// skip dest nodes that are not in this district
-            if (inSet(seen, neighPair.second)) continue; // skip visited
+            if (seenBool[neighPair.second]) continue; // skip visited
             heap.push(neighPair);
         }
     }
@@ -131,7 +159,6 @@ int prim_mst_per_district(vector<vector<wd_pair>> &adj, vector<int> &districts, 
 vector<int> fast_assign(vector<vector<pair<int, int>>> &adj, int D) {
     vector<int> districts(adj.size());
     vector<int> traversals(adj.size());
-    unordered_set<int> seen;
     queue<int> q;
     for (int i = 1; i <= D; ++i) {
         traversals[i] = 0;
@@ -139,16 +166,15 @@ vector<int> fast_assign(vector<vector<pair<int, int>>> &adj, int D) {
         q.push(i);
     }
     while (!q.empty()) {
+//        cout << q.size() << "q   " << seen.size() << " s   " << adj.size() << " adj\n";
         int curr = q.front(); q.pop();
         for(auto nei : adj[curr]) {
-            if(!inSet(seen, nei.second)) {
                 if(districts[nei.second] == 0) {
                     traversals[nei.second] = traversals[curr] + 1;
                     districts[nei.second] = districts[curr];
+//                    setbuf(stdout, 0); cout << curr; cout << "    " << nei.second; cout << " " << traversals[nei.second];                    cout << "|" << districts[nei.second] <<"\n";
+                    q.push(nei.second);
                 }
-                q.push(nei.second);
-                seen.insert(nei.second);
-            }
         }
     }
     return districts;
--- 01_calamity/fast_dist_assign.cpp
+++ 01_calamity/fast_dist_assign.cpp
@@ -7,21 +7,71 @@
 #include <queue>
 #include <unordered_set>
 #include <algorithm>
+#include <stack>
+#include <chrono>
 
+using namespace std::chrono;
 using namespace std;
 using wd_pair = pair<int, int>;
 
 bool inSet(unordered_set<int> set, int v);
 void getInput(int R, vector<vector<pair<int,int>>>& adj);
 void fast_assign(vector<vector<pair<int, int>>> &adj, int D);
+vector<int> ts(vector<vector<pair<int, int>>> &adj);
+void print_time(const char *string, std::chrono::time_point<std::chrono::system_clock> point);
 
 int main() {
+    std::chrono::time_point<std::chrono::system_clock> start = high_resolution_clock::now();
     int T, D, R; // V < 250k, D-towns < 2000, E < 450k
     cin >> T >> D >> R;
     vector<vector<pair<int,int>>> adj(T+1); // src -> { {w1, dest1}, {w2, dest2} }
     getInput(R, adj);
     fast_assign(adj, D);
+    print_time("fastassign ends at: ", start);
+    ts(adj);
+    print_time("ts ends at: ", start);
+
+
 }
+
+vector<int> ts(vector<vector<pair<int, int>>> &adj) {
+    int WHITE = 1; // fresh
+    int GRAY = 2;  // curr ent
+    int BLACK = 3; // closed
+
+    vector<int> colors(adj.size(), WHITE);
+    stack<int> stack;
+    vector<int> sorted;
+
+    for (int i = 0; i < adj.size(); i++) {
+        if (colors[i] != WHITE) continue;
+        stack.push(i);
+
+        while (!stack.empty()) {
+            int curr = stack.top();
+
+            if (colors[curr] == WHITE) { // 1) nenavstiveny node zabarvime
+                colors[curr] = GRAY;
+            }
+            else { // 5) nyni je uzel sedivy -> sousedy jsme zpracovali
+                colors[curr] = BLACK;
+                stack.pop();
+                sorted.push_back(curr);
+            }
+
+            for (pair<int, int> nei : adj[curr]) {      // 2) projdem sousedy
+                if (colors[nei.second] == GRAY) {   // 3) pokud je soused zabarveny, nasli jsme cyklus
+//                    return {}; /// tohle mozna zmenit pokud delame Kosaraju
+                } else if (colors[nei.second] == WHITE) { // 4) fresh souseda hodime na stack pro DFS
+                    stack.push(nei.second);
+                }
+            }
+            /// WARN: uzel jsme jeste nepopnuli!
+        }
+    }
+    return sorted;
+}
+
 
 void fast_assign(vector<vector<pair<int, int>>> &adj, int D) {
     vector<int> districts(adj.size());
@@ -36,15 +86,16 @@ void fast_assign(vector<vector<pair<int, int>>> &adj, int D) {
     while (!q.empty()) {
         int curr = q.front(); q.pop();
         for(auto nei : adj[curr]) {
-            if(!inSet(seen, nei.second)) {
+//            if(!inSet(seen, nei.second)) {
                 if(districts[nei.second] == 0) {
                     traversals[nei.second] = traversals[curr] + 1;
                     districts[nei.second] = districts[curr];
-    setbuf(stdout, 0); cout << curr; cout << "    " << nei.second; cout << " " << traversals[nei.second];                    cout << "|" << districts[nei.second] <<"\n";
+//    setbuf(stdout, 0); cout << curr; cout << "    " << nei.second; cout << " " << traversals[nei.second];                    cout << "|" << districts[nei.second] <<"\n";
+                    q.push(nei.second);
+                    seen.insert(nei.second);
                 }
-                q.push(nei.second);
-                seen.insert(nei.second);
-            }
+
+//            }
         }
     }
 }
@@ -52,6 +103,14 @@ void fast_assign(vector<vector<pair<int, int>>> &adj, int D) {
 bool inSet(unordered_set<int> set, int v) {
     if(set.find(v) != set.end()) return true;
     return false;
+}
+
+void print_time(const char *string, std::chrono::time_point<std::chrono::system_clock> point) {
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<nanoseconds>(stop - point);
+    setbuf(stdout, 0);
+    cout << string;
+    cout << duration.count()*0.000000001 << " s \n";
 }
 
 void getInput(int R, vector<vector<pair<int,int>>>& adj) {
