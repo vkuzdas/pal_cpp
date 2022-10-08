20bed545205cb315c192cab73c080b0e9aa8d39e
awesome-vojta <vojtechkuzdas@gmail.com>
2022-10-08 12:53:14 +0200

    BRUTE-2.0: tarjan passuje pri gcc-complete i memchecku a na vsech vstupech

--- 01_calamity/feed_pubs.sh
+++ 01_calamity/feed_pubs.sh
@@ -1,3 +1,8 @@
 for i in 01 02 03 04 05 06 07 08 09 10 ; do
-	./a.out <./datapub/pub${i}.in
+	./a.out < ./datapub/pub${i}.in > out
+  if ! diff ./datapub/pub${i}.out out ; then
+    echo "CHYBA pub${i}.in
+    "
+#    exit 1
+  fi
 done
---
+++ 02_pooldel/pooldel.cpp
@@ -0,0 +1,105 @@
+//
+// Created by kuzdavoj@fel.cvut.cz on 10/7/2022.
+//
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+
+using namespace std;
+
+vector<vector<unsigned int>> getInput(unsigned int N, unsigned int M);
+vector<vector<unsigned int>> tarjan_scc(vector<vector<unsigned int>> &adj, unsigned int N);
+
+int main() {
+    // 1) identify SCC's
+    // 2) detect WC's
+    // 3) DFS in MAX(var) to get min(cost)
+    unsigned int N, M;
+    cin >> N >> M;
+    vector<vector<unsigned int>> adj = getInput(N, M);
+    vector<vector<unsigned int>> comps = tarjan_scc(adj, N);
+    cout << comps.size();
+
+    return 0;
+}
+
+vector<vector<unsigned int>> tarjan_scc(vector<vector<unsigned int>> &adj, unsigned int N) { // consider const
+    const unsigned int UNDEF = 4294967295;
+    vector<unsigned int> index(N, UNDEF);
+    vector<unsigned int> lowlink(N, UNDEF);
+    vector<bool> on_stack(N, false);
+    stack<unsigned int> stck;
+    vector<vector<unsigned int>> comps;
+    stack<pair<unsigned int, unsigned int>> call_stack; // recursion argument stack
+    vector<unsigned int> scc_of(N, UNDEF);
+    unsigned int i = 0;
+
+    for (unsigned int v = 0; v < N; ++v) {
+        if (index[v] != UNDEF) continue;
+        call_stack.push({v, 0});
+        while (!call_stack.empty()) {
+            v = call_stack.top().first;
+            unsigned int pi = call_stack.top().second;
+            call_stack.pop();
+            // uzel nenavstiven
+            if (pi == 0) {
+                index[v] = i;
+                lowlink[v] = i;
+                i++;
+                stck.push(v);
+                on_stack[v] = true;
+            }
+            // "recursed" on something
+            if (pi > 0) {
+                unsigned int prev = adj[v][pi - 1];
+                lowlink[v] = min(lowlink[v], lowlink[prev]);
+            }
+            // recurse on
+            while (pi < adj[v].size() && index[adj[v][pi]] != UNDEF) {
+                unsigned int next = adj[v][pi];
+                if (on_stack[next]) {
+                    lowlink[v] = min(lowlink[v], index[next]);
+                }
+                pi++;
+            }
+            // found vert without index
+            if (pi < adj[v].size()) {
+                unsigned int next = adj[v][pi];
+                call_stack.push({v, pi + 1});
+                call_stack.push({next, 0});
+                continue;
+            }
+            // vertex is root of SCC
+            if (lowlink[v] == index[v]) {
+                vector<unsigned int> scc;
+                while (true) {
+                    auto next = stck.top();
+                    stck.pop();
+                    on_stack[next] = false;
+                    scc.push_back(next);
+                    if (next == v) {
+                        break;
+                    }
+                }
+                comps.push_back(scc);
+            }
+        }
+    }
+    return comps;
+}
+
+
+
+vector<vector<unsigned int>> getInput(unsigned int N, unsigned int M) {
+    vector<vector<unsigned int>> adj(N);
+    // reads graph from input and represents it as directed graph in adj list
+    for (unsigned int i = 0; i < M; ++i) {
+        unsigned int src, dest;
+        cin >> src >> dest;
+        adj[src].push_back(dest);
+    }
+    return adj;
+}
