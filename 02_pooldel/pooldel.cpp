0fae117bea082c72bc7281f3d613b1d9b02fe26f
awesome-vojta <vojtechkuzdas@gmail.com>
2022-10-14 09:52:12 +0200

    BRUTE-2.3: delete_wcs almost OK, (pub07 not wrong)

--- 02_pooldel/pooldel.cpp
+++ 02_pooldel/pooldel.cpp
@@ -13,123 +13,152 @@ using namespace std;
 
 vector<vector<unsigned int>> getInput(unsigned int N, unsigned int M);
 vector<vector<unsigned int>> tarjan_scc(vector<vector<unsigned int>> &adj, unsigned int N);
-void delete_wcs(vector<vector<unsigned int>> &comps, vector<vector<unsigned int>> &adj);
-void count_var_cost(
+void delete_wcs(vector<vector<unsigned int>> &comps,
+                vector<vector<unsigned int>> &adj,
+                vector<unsigned int> &var_indices,
+                unsigned int &max_var
+                );
+void get_min_cost(
         vector<vector<unsigned int>> &comps,
         vector<vector<unsigned int>> &adj,
         vector<unsigned int> &var,
         vector<unsigned int> &cost
         );
 
+
+// 1) identify SCC's
+// 2) detect WC's
+// 3) DFS in MAX(var) to get min(cost)
 int main() {
-    // 1) identify SCC's
-    // 2) detect WC's
-    // 3) DFS in MAX(var) to get min(cost)
     unsigned int N, M;
     cin >> N >> M;
 
     vector<vector<unsigned int>> adj = getInput(N, M);
     vector<vector<unsigned int>> comps = tarjan_scc(adj, N);
-    delete_wcs(comps, adj);
-    vector<unsigned int> var(comps.size(),0), cost(comps.size(),0);
-    count_var_cost(comps, adj, var, cost);
 
-    cout << comps.size();
+    unsigned int max_var = 0;
+    vector<unsigned int> var_indices;
+    delete_wcs(comps, adj, var_indices, max_var);
+
+
+    vector<unsigned int> cost(comps.size(),0);
+    get_min_cost(comps, adj, var_indices, cost);
+
+    cout << "comps size: " << comps.size() << "\n";
+    cout << "var: " << max_var << "\n";
     return 0;
 }
 
-void count_var_cost(
-        vector<vector<unsigned int>> &comps,
-        vector<vector<unsigned int>> &adj,
-        vector<unsigned int> &var,
-        vector<unsigned int> &cost
-               ) {
-    for (unsigned int i = 0; i < comps.size(); ++i) {
-        // var = all nodes that are not UINT_MAX
-        vector<unsigned int> scc = comps[i];
-        unsigned int min_cost =
-        // dfs to all nodes, track only the minimal
 
-    }
-}
+//1c1,2
+//< 2 94 3165
+//---
+//> comps size: 5
+//> var: 118        should be 94!
+/// pub07 actin' weird
 
+/**
+ * There are nodes that have an edge going out from SCC these edges are "weak crossings".
+ * We mark them by replacing their node ID with UINT_MAX.
+ *
+ * For efficiency purposes we also find the maximum size of SCC without weak crossings (@param max_var).
+ * Moreover, the position of these SCCs is marked within @param var_indices.
+ */
+void delete_wcs(vector<vector<unsigned int>> &comps,
+                vector<vector<unsigned int>> &adj,
+                vector<unsigned int> &var_indices,
+                unsigned int &max_var) {
+    vector<unsigned int> comp_indices;
 
-void delete_wcs(vector<vector<unsigned int>> &comps, vector<vector<unsigned int>> &adj) {
-    for (unsigned int i = 0; i < comps.size(); ++i) {
-        vector<unsigned int> scc = comps[i];
-        vector<unsigned int> index_to_delete;
-        for (unsigned int j = 0; j < scc.size(); ++j) {
-            unsigned int src = scc[j];
-            for (unsigned int k = 0; k < adj[src].size(); ++k) {
+    for (unsigned int i = 0; i < comps.size(); ++i) { /// for a comp
+        vector<unsigned int> SCC = comps[i];
+        vector<unsigned int> weak_indices;
+        for (unsigned int j = 0; j < SCC.size(); ++j) { /// for a node within comp
+            unsigned int src = SCC[j];
+            for (unsigned int k = 0; k < adj[src].size(); ++k) { /// for node's dest
                 unsigned int dst = adj[src][k];
-                if(std::find(scc.begin(), scc.end(), dst) == scc.end()) {
-                    index_to_delete.push_back(j);
+                if(std::find(SCC.begin(), SCC.end(), dst) == SCC.end()) { // FIXME: might be slow
+                    // destination of crossing is not within SCC -> should be deleted
+                    printf("Del %d from comp[%d] (%d -> %d)\n", src, i, src, dst);
+                    weak_indices.push_back(j);
+                    break;
                 }
             }
         }
-        for (unsigned int index : index_to_delete) {
-            scc[index] = UINT_MAX;
+        for (unsigned int index : weak_indices) {
+            // weak crossing "deletion" (marking is faster)
+            SCC[index] = UINT_MAX;
         }
-        comps[i] = scc;
+        comps[i] = SCC;
+        auto comp_size = (unsigned int)(SCC.size() - weak_indices.size());
+        if (comp_size > max_var) {
+            // found new max -> delete indices of lower var, push index of curr comp
+            max_var = comp_size;
+            var_indices.clear();
+            var_indices.push_back(i);
+        } else if (comp_size == max_var) {
+            // var is still the same, mark its index
+            var_indices.push_back(i);
+        }
     }
+    if (max_var != 0) max_var = max_var - 1; /// var is the num of nodes that each node can get to in a SCC
 }
 
 // TODO: docs
 vector<vector<unsigned int>> tarjan_scc(vector<vector<unsigned int>> &adj, unsigned int N) { // consider const
     const unsigned int UNDEF = UINT_MAX;
-    vector<unsigned int> index(N, UNDEF);
+    vector<unsigned int> index(N, UNDEF); // order of node in which it was visited
     vector<unsigned int> lowlink(N, UNDEF);
     vector<bool> on_stack(N, false);
     stack<unsigned int> stck;
     vector<vector<unsigned int>> comps;
     stack<pair<unsigned int, unsigned int>> call_stack; // recursion argument stack
-    vector<unsigned int> scc_of(N, UNDEF);
     unsigned int i = 0;
 
-    for (unsigned int v = 0; v < N; ++v) {
-        if (index[v] != UNDEF) continue;
-        call_stack.push({v, 0});
+    for (unsigned int node = 0; node < N; ++node) {
+        if (index[node] != UNDEF) continue;
+        call_stack.push({node, 0});
         while (!call_stack.empty()) {
-            v = call_stack.top().first;
-            unsigned int pi = call_stack.top().second;
+            node = call_stack.top().first;
+            unsigned int ptr = call_stack.top().second;
             call_stack.pop();
             // uzel nenavstiven
-            if (pi == 0) {
-                index[v] = i;
-                lowlink[v] = i;
+            if (ptr == 0) {
+                index[node] = i;
+                lowlink[node] = i;
                 i++;
-                stck.push(v);
-                on_stack[v] = true;
+                stck.push(node);
+                on_stack[node] = true;
             }
             // "recursed" on something
-            if (pi > 0) {
-                unsigned int prev = adj[v][pi - 1];
-                lowlink[v] = min(lowlink[v], lowlink[prev]);
+            if (ptr > 0) {
+                unsigned int prev = adj[node][ptr - 1];
+                lowlink[node] = min(lowlink[node], lowlink[prev]);
             }
             // recurse on
-            while (pi < adj[v].size() && index[adj[v][pi]] != UNDEF) {
-                unsigned int next = adj[v][pi];
+            while (ptr < adj[node].size() && index[adj[node][ptr]] != UNDEF) {
+                unsigned int next = adj[node][ptr];
                 if (on_stack[next]) {
-                    lowlink[v] = min(lowlink[v], index[next]);
+                    lowlink[node] = min(lowlink[node], index[next]);
                 }
-                pi++;
+                ptr++;
             }
             // found vert without index
-            if (pi < adj[v].size()) {
-                unsigned int next = adj[v][pi];
-                call_stack.push({v, pi + 1});
+            if (ptr < adj[node].size()) {
+                unsigned int next = adj[node][ptr];
+                call_stack.push({node, ptr + 1});
                 call_stack.push({next, 0});
                 continue;
             }
             // vertex is root of SCC
-            if (lowlink[v] == index[v]) {
+            if (lowlink[node] == index[node]) {
                 vector<unsigned int> scc;
                 while (true) {
                     auto next = stck.top();
                     stck.pop();
                     on_stack[next] = false;
                     scc.push_back(next);
-                    if (next == v) {
+                    if (next == node) {
                         break;
                     }
                 }
@@ -139,8 +168,6 @@ vector<vector<unsigned int>> tarjan_scc(vector<vector<unsigned int>> &adj, unsig
     }
     return comps;
 }
-
-
 
 vector<vector<unsigned int>> getInput(unsigned int N, unsigned int M) {
     vector<vector<unsigned int>> adj(N);
