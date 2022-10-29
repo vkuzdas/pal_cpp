66645c8c49a757700b588cfa2fee57944f62e3ae
awesome-vojta <vojtechkuzdas@gmail.com>
2022-10-29 21:49:22 +0200

    BRUTE-3.4: d1 & d2 fungujou na pub01, jinak to vypada ze to nikde nestavkuje na zadnem pubu

--- 03_netw/netw.cpp
+++ 03_netw/netw.cpp
@@ -7,11 +7,13 @@
 #include <vector>
 #include <algorithm>
 #include <queue>
+#include <map>
 
 using namespace std;
 using uint = unsigned int;
 using wd_pair = pair<uint, uint>;
 
+// input
 vector<vector<uint>> get_old_net(uint n, uint m) {
     // reads graph from input and represents it as mirrored directed graph in adj list
     vector<vector<uint>> adj(n);
@@ -24,6 +26,7 @@ vector<vector<uint>> get_old_net(uint n, uint m) {
     return adj;
 }
 
+// input
 vector<vector<wd_pair>> get_new_net(uint vert_count, uint edge_count) {
     // reads graph from input and represents it as mirrored directed graph in adj list
     vector<vector<wd_pair>> adj(vert_count);
@@ -36,6 +39,7 @@ vector<vector<wd_pair>> get_new_net(uint vert_count, uint edge_count) {
     return adj;
 }
 
+// input
 void get_fast_servers(uint f, vector<bool> &is_fast, vector<uint> &f_servers) {
     for (uint i = 0; i < f; ++i) {
         uint edge_label;
@@ -85,6 +89,7 @@ uint bin_coeff(uint n, uint k) {
     return (uint)(res + 0.01);
 }
 
+// fce  rank->subset
 vector<uint> unrank_subset(uint rank, uint n, uint k) {
     if (k == 1) {
         return {rank+1};
@@ -110,41 +115,81 @@ vector<uint> unrank_subset(uint rank, uint n, uint k) {
     return subset_rec;
 }
 
-vector<uint> get_old_degrees_sorted(vector<vector<uint>> &old_g) {
+// node-id: stupen[D1 je stupen uzlu]
+map<uint,uint> get_old_d1(vector<vector<uint>> &old_g) {
+    map<uint,uint> node_degree;
     vector<uint> old_degrees;
-    for (auto const& src : old_g) {
-        old_degrees.push_back((uint)src.size());
+    for (uint i = 0; i < old_g.size(); ++i) {
+        uint src = i;
+        uint degree = (uint)old_g[i].size();
+        node_degree[src] = degree;
     }
-    std::sort(old_degrees.begin(), old_degrees.end());
-    return old_degrees;
+    return node_degree;
 }
 
-bool degrees_ok(vector<bool> &in_subset, vector<uint> &curr_subset, vector<vector<wd_pair>> &new_g, vector<vector<uint>> &old_g, vector<uint> &old_degrees_sorted) {
-    // projdi vsechny uzly ze subsetu
-    // koukni jaky degree maji uvnitr new grafu
-    // odecti od degree ty ktere nejsou v subsetu
+// node-id: stupen [D1 je stupen uzlu]
+map<uint,uint> get_new_d1(vector<vector<wd_pair>> &new_g, vector<bool> &in_subset, vector<uint> &curr_subset) {
     vector<uint> new_degrees;
+    map<uint,uint> node_degree;
     for (uint i = 0; i < curr_subset.size(); ++i) {
         uint curr_node = curr_subset[i];
         vector<wd_pair> neighbors = new_g[curr_node];
         uint degree = 0;
         for (uint j = 0; j < neighbors.size(); ++j) {
             uint neigh = neighbors[j].second;
-            if (in_subset[neigh]) {
+            if (in_subset[neigh]) { // pocitej pouze uzly ktere jsou v subsetu
                 degree++;
             }
         }
-        new_degrees.push_back(degree);
+        node_degree[curr_node] = degree;
     }
-    std::sort(new_degrees.begin(), new_degrees.end());
-    for (uint i = 0; i < old_degrees_sorted.size(); ++i) {
-        if (old_degrees_sorted[i] != new_degrees[i]) {
-            return false;
-        }
-    }
-    return true;
+    return node_degree;
 }
 
+// node-id: vector stupnu
+// vector stupnu:  pozice = stupen, hodnota = pocet uzlu s takovym stupnem
+map<uint, vector<uint>> get_old_d2(vector<vector<uint>> &old_g) {
+    map<uint, vector<uint>> node_d2;
+    for (uint i = 0; i < old_g.size(); ++i) { // pro node
+        vector<uint> d2(10, 0); // 10 nodu, zadne degrees
+        vector<uint> neighbors = old_g[i];
+        for (uint j = 0; j < neighbors.size(); ++j) { // pro jeho sousedy
+            uint neighbor = neighbors[j];
+            auto neighbor_degree = old_g[neighbor].size(); // neighbor ma treba d1=3 -> zvedneme pocet 3-deg uzlu
+            d2[neighbor_degree]++;
+        }
+        node_d2[i] = d2;
+    }
+    return node_d2;
+}
+
+// node-id: vector stupnu
+// vector stupnu:  pozice = stupen, hodnota = pocet uzlu s takovym stupnem
+map<uint, vector<uint>> get_new_d2(vector<vector<wd_pair>> &new_g, vector<uint> &curr_subset, vector<bool> &in_subset) {
+    map<uint, vector<uint>> node_d2;
+    for (uint i = 0; i < curr_subset.size(); ++i) {
+        vector<uint> d2(10, 0);
+        uint curr_node = curr_subset[i];
+        auto neighbors = new_g[curr_node];
+        for (uint j = 0; j < neighbors.size(); ++j) { // projed vsechny neigh ktere jsou v setu
+            uint neighbor = neighbors[j].second;
+            if(!in_subset[neighbor]) continue;
+            // od neigh degree se musi odecist ty ktere tam nejsou
+            uint nei_degree = 0;
+            for (uint k = 0; k < new_g[neighbor].size(); ++k) { // koukni na neighbors' neighbors, jestli jsou v setu, inkrementuj degree
+                if(in_subset[new_g[neighbor][k].second]) {
+                    nei_degree++;
+                }
+            }
+            d2[nei_degree]++;
+
+        }
+        node_d2[curr_node] = d2;
+    }
+    return node_d2;
+}
+
+// BFS grafem, pokud se dostaneme do vsech nodu, vraci true
 bool is_connected(vector<bool> &in_subset, vector<uint> &curr_subset, vector<vector<wd_pair>> &new_g) {
     // udelej jedno BFS z nejakeho uzlu v curr_subset
     // do souseda vejdi jen pokud je v curr_subset
@@ -174,41 +219,47 @@ bool is_connected(vector<bool> &in_subset, vector<uint> &curr_subset, vector<vec
     return true;
 }
 
-bool invariants_ok(vector<uint> &curr_subset, vector<vector<wd_pair>> &new_g, vector<vector<uint>> &old_g, vector<uint> &old_degrees_sorted) {
-    vector<bool> in_subset(30, false);
-    for (auto node : curr_subset) in_subset[node] = true;
+// TODO: kontrola pravidla sousednosti
+map<uint, uint> find_mapping(
+        vector<bool> &in_subset,
+        vector<uint> &curr_subset,
+        vector<vector<wd_pair>> &new_g,
+        vector<vector<uint>> &old_g,
+        map<uint,uint> &d1_old_map,
+        map<uint,vector<uint>> &d2_old
+    ) {
+    // zkontroluj ze d1 stupne uzlu jsou stejne
+    vector<uint> d1_old_sorted;
+    for (auto p : d1_old_map) d1_old_sorted.push_back(p.second);
 
-    // check degrees
-    if(!degrees_ok(in_subset, curr_subset, new_g, old_g, old_degrees_sorted)) {
-        cout << "bad degrees. \n";
-        return false;
-    } else {cout << "ok degrees, ";}
-    // check for connectivity
-    if(!is_connected(in_subset, curr_subset, new_g)) {
-        cout << "disconnected.\n";
-        return false;
-    } else {cout << "connected. \n";}
-    // check if it is a tree [connected and n-1 edges]
-    // check if it is planar
-    // match neighbour old_degrees
-    return false;
+    map<uint,uint> d1_new_map = get_new_d1(new_g, in_subset, curr_subset);
+    vector<uint> d1_new_sorted;
+    for (auto p : d1_new_map) d1_new_sorted.push_back(p.second);
+
+    sort(d1_old_sorted.begin(), d1_old_sorted.end());
+    sort(d1_new_sorted.begin(), d1_new_sorted.end());
+    for (uint i = 0; i < d1_old_sorted.size(); ++i) {
+        if (d1_old_sorted[i] != d1_new_sorted[i]) {
+            cout << "d1 mapping NOT ok. \n";
+            return {{}};
+        }
+    }
+    cout << "d1 mapping ok. \n";
+
+    map<uint, vector<uint>> d2_new = get_new_d2(new_g, curr_subset, in_subset);
+
+
+    // rozdel uzly do skupin dle degrees
+    // skupiny z old a new porovnej
+    // meli by byt shodne
+    // rozedl uzly do skupin dle neighbor degrees
+    // prmutace uvnitr skupin daji vhodne mappings
+    // pro kazdy mapping:
+    // zkontroluj kazdou hranu v mappingu zda existuje v novem i starem grafu
+
+    return {{0,0}};
 }
 
-//bool neighbors_ok(vector<uint> &curr_subset, vector<vector<wd_pair>> &new_g, vector<vector<uint>> &old_g) {
-//     pravidlo: uzly ktere nesousedili v old nesmi sousedit v new
-//     tohle se bude checkovat po vygenerovani mappingu
-//    return false;
-//}
-
-//Priblizny postup:
-// ✅ Premapujeme vsechny uzly v New tak aby FAST byly na zacatku
-//    generujeme k-subsety (protoze jsme premapovali tak prvni subsety jsou s nejvice FAST)
-//    pro kazdy subset (bincoef(S,k)-krat) zkontrolujeme Invarianty
-//      [shoda degrees, pravidlo sousedu ze zadani, planarita, spojitost, ...]
-//    Pokud se v subsetu shoduji invarianty zjisti zda existuje mapping
-//    Pokud existuje mapping, zapamatuj si pocet jeho F
-//    Dale generuj pouze s timto poctem F
-//    nakonec pro kazdy mapping spocti cenu
 
 int main() {
     // get old
@@ -226,20 +277,45 @@ int main() {
 
     // remapuj aby fast servery byly na prvnich mistech -> generovat budeme nejdrive MAX(F)
     new_g = remap_new_g(new_g, is_fast, f_servers);
-
     // kombinacni cislo: n1-servrové kombinace z n2 serveru [napr 5 serveru z 10]
     uint last_rank = bin_coeff(n2, n1);
-    vector<uint> old_degrees_sorted = get_old_degrees_sorted(old_g);
+    map<uint,uint> old_d1 = get_old_d1(old_g);
+    map<uint,vector<uint>> old_d2 = get_old_d2(old_g);
     for (uint i = 0; i < last_rank; ++i) { // TODO: teoreticky by se dalo jit z obou koncu (prevratit mapping)
+
         vector<uint> candidate = unrank_subset(i, n2, n1);
         for (uint j = 0; j < candidate.size(); ++j) candidate[j] = candidate[j]-1; // TODO: linear time!
 
-        cout << i << "th candidate:  [";
+        cout << i << " / " << last_rank << " candidate:  [";
         for (uint j = 0; j < candidate.size(); ++j) cout << candidate[j] << ", ";
         cout << "]  ";
 
-        invariants_ok(candidate, new_g, old_g, old_degrees_sorted);
+        vector<bool> in_subset(30, false);
+        for (auto node : candidate) in_subset[node] = true;
+        if(!is_connected(in_subset, candidate, new_g)) {
+            setbuf(stdout, nullptr);
+            cout << "disconnected.\n";
+            continue;
+        } else {
+            setbuf(stdout, nullptr);
+            cout << "connected, ";
+        }
+
+        map<uint, uint> mapping = find_mapping(
+                in_subset, candidate,new_g,
+                old_g, old_d1, old_d2
+            );
     }
 
     return 0;
 }
+
+//  Priblizny postup:
+//   ✅ Premapujeme vsechny uzly v New tak aby FAST byly na zacatku
+//   ✅ generujeme k-subsety (protoze jsme premapovali tak prvni subsety jsou s nejvice FAST)
+//   ✅ pro kazdy subset (bincoef(S,k)-krat) zkontrolujeme Invarianty
+//        [shoda degrees, pravidlo sousedu ze zadani, planarita, spojitost, ...]
+//  ->  Pokud se v subsetu shoduji invarianty zjisti zda existuje mapping
+//      Pokud existuje mapping, zapamatuj si pocet jeho F
+//      Dale generuj pouze s timto poctem F
+//      nakonec pro kazdy mapping spocti cenu
