1bdd3a9cc78a91427ef00d447a2ccbaf512ab925
awesome-vojta <vojtechkuzdas@gmail.com>
2022-11-16 08:56:58 +0100

    BRUTE-4.1: hruba implementace selekce dalsiho prime z predchozich

--- 04_cnt_leh/cnt.cpp
+++ 04_cnt_leh/cnt.cpp
@@ -13,6 +13,8 @@ using uint = unsigned int;
 using ulli = unsigned long long int;
 using namespace std::chrono;
 
+
+
 const bool DBG_PRINT = true;
 
 
@@ -35,6 +37,20 @@ size_t prime_size(ulli n) {
     }
 }
 
+void get_root() {
+
+}
+
+/// D je z rozsahu <3,40>
+vector<uint> get_p_under_D(uint D) {
+    vector<uint> ps_under_DMAX{2,3,5,7,11,13,17,19,23,29,31,37};
+    if(D>=37) return ps_under_DMAX;
+    for (uint i = 0; i < ps_under_DMAX.size(); ++i) {
+        if (D < ps_under_DMAX[i])
+            return vector<uint>(ps_under_DMAX.begin(),ps_under_DMAX.begin()+i);
+    }
+    return {};
+}
 
 vector<uint> sift_under_sqrt(ulli n) {
     // staci nam cisla do sqrt(M_max)
@@ -57,7 +73,7 @@ vector<uint> sift_under_sqrt(ulli n) {
 
     end = std::chrono::system_clock::now();
     std::chrono::duration<double> elapsed_seconds = end - start;
-    cout << "sifted in " << elapsed_seconds.count() << "ms\n";
+    if(DBG_PRINT) cout << "sifted in " << elapsed_seconds.count() << "ms\n";
     vector<uint> primes;
     for (uint i = 2; i < A.size(); ++i) {
         if(A[i]) primes.push_back(i);
@@ -72,40 +88,60 @@ vector<uint> sift_under_sqrt(ulli n) {
 ///    ######################
 
 
+
 int main() {
-    ulli M_max, D;
+    ulli M_max; uint D;
     cin >> M_max >> D;
 
+    vector<uint> p_under_D = get_p_under_D(D);
+    vector<uint> p_under_sqrt = sift_under_sqrt(M_max);
+    ulli count = p_under_sqrt.size();
+    uint last_prime = p_under_sqrt[p_under_sqrt.size() - 1];
 
-
-    vector<uint> primes = sift_under_sqrt(M_max);
-//    print_vec(primes, "primes");
-    ulli count = primes.size();
-
-
-    chrono::time_point<chrono::system_clock> start, end;
-    start = chrono::system_clock::now();
-
-    uint last_prime = primes[primes.size()-1];
-    for (ulli candidate = last_prime+1; candidate < M_max; ++candidate) {
-        /// per candidate
-
-
+    /// per candidate M: M-1 |? PF()>D
+    for (ulli M = last_prime + 1; M < M_max; ++M) {
         bool is_prime = true;
-        for (auto p : primes) {
-            // ozkousej zda kandidat je delitelny prvocislama z primes under sqrt
-            // je-li kandidat delitelny, neni prvocislo
-            if(candidate % p == 0) {
+        bool ok_root = true;
+        vector<ulli> PF;
+//        vector<uint> more_primes = ( p_under_sqrt.size() > p_under_D.size() ) ? p_under_sqrt : p_under_D;
+
+        /// iteruj do p_under_D/p_under_sqrt - podle toho co je delsi
+        for (auto p : p_under_sqrt) { // TODO: kdyz PUR neobsahuje primes do D? nebo nad D?
+            ulli R_res = (M-1) % p;
+            ulli M_res = M % p;
+
+            /// alespon jedno p <= D musi delit M-1
+            if(p > D && PF.empty()) {
+                ok_root = false;
+                break;
+            }
+
+            /// p nesmi delit M aby M byl prime,
+            if (M_res == 0) {
                 is_prime = false;
                 break;
             }
+
+            /// p > D NESMI delit M-1
+            if(R_res == 1 && p > D) {
+                ok_root = false;
+                break;
+            }
+            else if(R_res == 0) {
+                if(p <= D) {
+                    PF.push_back(p);
+                }
+                else if(p > D) {
+                    ok_root = false;
+                    break;
+                }
+            }
         }
-        if (is_prime) count++;
-
-
+        if (is_prime && ok_root) {
+            count++;
+            if (DBG_PRINT) cout << "Prime with root: " << M << endl;
+            get_root();
+        }
     }
-
-    end = chrono::system_clock::now();
-    chrono::duration<double> elapsed_seconds = end - start;
-    cout << "candidates in " << elapsed_seconds.count() << "ms \n";
+    print_vec(p_under_sqrt, "PUR");
 }
