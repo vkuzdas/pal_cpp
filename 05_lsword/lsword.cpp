16e9cd768d93e927674af0ea36daf2c0eac4e7cc
awesome-vojta <vojtechkuzdas@gmail.com>
2022-12-03 08:46:08 +0100

    BRUTE-5.1: input reading getline whitespace split no SF

--- 05_lsword/lsword.cpp
+++ 05_lsword/lsword.cpp
@@ -22,34 +22,43 @@ using vs_pair = pair<vector<uint>,string>;
 const vector<char> from_int_char{'a','b','c','d','e'};
 
 
-vector<uint> readEdge(uint N, uint M) {
-    char edge_char, sep;
-    scanf("%c%c", &edge_char, &sep);
-    vector<uint> collect{};
-    if (sep == '\n') return collect; // pokud za pismenem je \n, jsme na konci radky
-    for (uint i = 0; i < M; ++i) {
-        uint edge_dest = UINT_MAX;
-        scanf("%d%c", &edge_dest,&sep);
-        if (edge_dest == UINT_MAX) return collect;
-        collect.push_back(edge_dest);
-        if (sep == '\n') return collect; // pokud za cislici je \n, jsme na konci radky
-    }
-    return collect;
+bool is_abcde(string s) {
+    if(s[0]=='a' || s[0]=='b' || s[0]=='c' || s[0]=='d' || s[0]=='e')
+        return true;
+    return false;
 }
 
 vector<vector<uint>> readLine(uint curr_N, uint N, uint M, vector<bool>& is_final) {
-    // format:  0 -  a 1    b 3   c
-    // nejdriv precti "0 - " tzn digit a char
-    int src_state;
-    scanf("%d ", &src_state); // src_state = curr N
-    char is_curr_final;
-    scanf("%c   ", &is_curr_final);
-    if (is_curr_final == 'F') is_final[curr_N] = true;
+    string line;
+    getline(cin, line);  // precte celej line, format:  0 -     a 1    b 3   c
+    istringstream iss(line);
 
-    vector<vector<uint>> edge_dest;
-    for (uint i = 0; i < M; ++i) {
-        // M-krat "a 2 5 1"
-        edge_dest.push_back(readEdge(N, M));
+    // extrahuj cislo stavu
+    string curr_state_str = "";
+    while(curr_state_str.empty()) {
+        getline(iss, curr_state_str, ' ');
+    }
+    int curr_state = stoi(curr_state_str);
+
+    // extrahuj is_final
+    string final_str;
+    getline(iss, final_str, ' ');
+    is_final[uint(curr_state)] = final_str[0] == 'F';
+
+
+    vector<vector<uint>> edge_dest{M};
+
+    // alespon M-krat precti
+    string token;
+    int subvec_index = -1;
+    while(getline(iss, token, ' ')) {
+        if (token.empty()) continue;
+        if (is_abcde(token)) {
+            subvec_index = token[0] - 'a';
+        } else {
+            uint dest_state = uint(stoi(token));
+            edge_dest[uint(subvec_index)].push_back(dest_state);
+        }
     }
     return edge_dest;
 }
@@ -106,15 +115,17 @@ vector<uint> BFS_step(vector<bool>& is_final, vector<vector<vector<uint>>>& nfa,
 }
 
 
-int _main() {
+int main() {
     uint N, M;   // N = pocet stavu, M = velikost abecedy
     cin >> N >> M;
 
-    vector<bool> is_final(M, false);
+    string e;getline(cin, e); /// read empty line
+
+    vector<bool> is_final(N, false);
     vector<vector<vector<uint>>> nfa;
     nfa = readNFA(N, M, is_final);
-//    string S;
-//    getline(cin, S);
+    string S;
+    getline(cin, S);
 
 //    vector<pair<vector<uint>,string>> found_seq;
 //    queue<uint> q;
@@ -131,24 +142,3 @@ int _main() {
     return 0;
 }
 
-int main() {
-    uint N, M;   // N = pocet stavu, M = velikost abecedy
-    cin >> N >> M;
-    string line;
-    getline(cin, line); // empty line
-    while(getline(cin, line)) {
-        vector<string> tokens;
-        std::istringstream iss(line);
-        string token;
-        while (std::getline(iss, token, ' ')) {
-            if(!token.empty()) {
-                tokens.push_back(token);
-            }
-        }
-    }
-
-
-
-    cout << "ok";
-
-}
