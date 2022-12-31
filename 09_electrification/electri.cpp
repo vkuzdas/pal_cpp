6c430ddf438b3add2b30f30cec59c96af1011abf
kimoki-boop <vojtechkuzdas@gmail.com>
2022-12-31 14:57:13 +0100

    ELECTRI: making sense of ham

--- 09_electrification/electri.cpp
+++ 09_electrification/electri.cpp
@@ -9,33 +9,32 @@
 
 using namespace std;
 
-typedef struct edge {
-    int predIdx;
-    int endIdx;
-    int value;
-    int edgeIdx;
+struct Edge {
+    int src;
+    int dest;
+    int cost;
 
-    bool operator<(const edge &rhs) const {
-        return value < rhs.value;
+    bool operator < (const Edge &e) const {
+        return cost < e.cost;
     }
 
-    bool operator>(const edge &rhs) const {
-        return value > rhs.value;
+    bool operator > (const Edge &e) const {
+        return cost > e.cost;
     }
-} edge;
+};
 
 
 
-typedef struct vert {
+struct Node {
     int parrent;
     int capacity;
     int val;
     int mark;
     vector<int> neib;
-} UFvert;
+};
 
 
-int find(vector<vert> &UF, int vert) {
+int find(vector<Node> &UF, int vert) {
     if (UF[vert].parrent == vert) {
         return vert;
     } else {
@@ -45,7 +44,7 @@ int find(vector<vert> &UF, int vert) {
 }
 
 
-void unite(vector<vert> &UF, int &counter, int v1, int v2, int eVal, bool force) {
+void unite(vector<Node> &UF, int &counter, int v1, int v2, int eVal, bool force) {
     // not same commponent
     int t1 = find(UF, v1);
     int t2 = find(UF, v2);
@@ -54,25 +53,21 @@ void unite(vector<vert> &UF, int &counter, int v1, int v2, int eVal, bool force)
             UF[t2].parrent = t1;
             UF[t1].capacity += UF[t2].capacity;
             UF[t1].val = UF[t1].val + eVal + UF[t2].val;
-
         } else {
             UF[t1].parrent = t2;
             UF[t2].capacity += UF[t1].capacity;
             UF[t2].val = UF[t2].val + eVal + UF[t1].val;
-
         }
         --counter;
     }
-
     if (t1 == t2 && force) {
         UF[t1].val += eVal;
-
         --counter;
     }
 }
 
 
-void dfs(vector<vert> &UF, int v, int mark) {
+void dfs(vector<Node> &UF, int v, int mark) {
     UF[v].mark = mark;
     for (auto e: UF[v].neib) {
         if (UF[e].mark == -1) dfs(UF, e, mark);
@@ -80,17 +75,16 @@ void dfs(vector<vert> &UF, int v, int mark) {
 }
 
 
-int find_b(vector<vert> &UFB, int vert) {
+int find_b(vector<Node> &UFB, int vert) {
     if (UFB[vert].parrent == vert) {
         return vert;
     } else {
         return UFB[vert].parrent = find_b(UFB, UFB[vert].parrent);
     }
-
 }
 
 
-bool unite_B(vector<vert> &UFB, int v1, int v2, int eVal, bool force) {
+bool unite_B(vector<Node> &UFB, int v1, int v2, int eVal) {
     // not same commponent
     int t1 = find_b(UFB, v1);
     int t2 = find_b(UFB, v2);
@@ -99,12 +93,10 @@ bool unite_B(vector<vert> &UFB, int v1, int v2, int eVal, bool force) {
             UFB[t2].parrent = t1;
             UFB[t1].capacity += UFB[t2].capacity;
             UFB[t1].val = UFB[t1].val + eVal + UFB[t2].val;
-
         } else {
             UFB[t1].parrent = t2;
             UFB[t2].capacity += UFB[t1].capacity;
             UFB[t2].val = UFB[t2].val + eVal + UFB[t1].val;
-
         }
         return true;
     }
@@ -112,60 +104,61 @@ bool unite_B(vector<vert> &UFB, int v1, int v2, int eVal, bool force) {
 }
 
 
-std::priority_queue<edge, std::vector<edge>, std::greater<edge> > eCrossRiv0;
-std::priority_queue<edge, std::vector<edge>, std::greater<edge> > eCrossRiv;
-std::priority_queue<edge, std::vector<edge>, std::greater<edge> > eOther;
+
 
 int counter;
 int comp = 0;
 
 int main() {
-    int M, N, D, B;
-    vector<UFvert> UF;
-    vector<UFvert> UFB;
+    priority_queue<Edge, vector<Edge>, greater<>> river_edges_PQ0;
+    priority_queue<Edge, vector<Edge>, greater<>> river_edges_PQ1;
+    priority_queue<Edge, vector<Edge>, greater<>> other_edges_PQ;
 
+    int M, N, D, B;
     cin >> M >> N >> D >> B;
     counter = N;
-    UFvert t;
-    t.neib = vector<int>();
-    t.val = 0;
-    t.mark = -1;
-    t.capacity = 1;
-    UF = vector<UFvert>(N, t);
-    for (int i = 0; i < M; i++) {
-        int V1, V2, C;
-        cin >> V1 >> V2 >> C;
 
-        edge tmp;
-        tmp.edgeIdx = i;
-        tmp.predIdx = V1;
-        tmp.endIdx = V2;
-        tmp.value = C;
+
+    Node tmp;
+    tmp.neib = vector<int>();
+    tmp.val = 0;
+    tmp.mark = -1;
+    tmp.capacity = 1;
+    vector<Node> nodes = vector<Node>(N, tmp);
+
+    for (int i = 0; i < M; i++) {
+        int src, dest, cost;
+        cin >> src >> dest >> cost;
+
+        Edge tmp;
+        tmp.src = src;
+        tmp.dest = dest;
+        tmp.cost = cost;
 
         // init Union find struct
-        UF[V1].parrent = V1;
-        UF[V2].parrent = V2;
+        nodes[src].parrent = src;
+        nodes[dest].parrent = dest;
 
         // check if is the river edge
-        if (V1 <= D && V2 > D || V1 > D && V2 <= D) {
+        if (src <= D && dest > D || src > D && dest <= D) {
             // add to river queue
-            eCrossRiv0.push(tmp);
+            river_edges_PQ0.push(tmp);
 
         } else {
             // add to other queue
-            eOther.push(tmp);
-            UF[V1].neib.push_back(V2);
-            UF[V2].neib.push_back(V1);
+            other_edges_PQ.push(tmp);
+            nodes[src].neib.push_back(dest);
+            nodes[dest].neib.push_back(src);
         }
     }
 
-    UFB = vector<UFvert>();
+    vector<Node> UFB = vector<Node>();
     // find Components
     for (int i = 0; i < N; i++) {
-        if (UF[i].mark == -1) {
+        if (nodes[i].mark == -1) {
 
-            dfs(UF, i, comp);
-            UFvert ut;
+            dfs(nodes, i, comp);
+            Node ut;
 
             ut.parrent = comp;
             ut.capacity = 1;
@@ -176,39 +169,39 @@ int main() {
     }
 
 
-    while (eCrossRiv0.size() > 0) {
-        edge tmp = eCrossRiv0.top();
-        eCrossRiv0.pop();
+    while (!river_edges_PQ0.empty()) {
+        Edge tmp = river_edges_PQ0.top();
+        river_edges_PQ0.pop();
 
         // if i can add it
-        if (unite_B(UFB, UF[tmp.predIdx].mark, UF[tmp.endIdx].mark, tmp.value, 1)) {
+        if (unite_B(UFB, nodes[tmp.src].mark, nodes[tmp.dest].mark, tmp.cost)) {
 
             // if i can add it
-            unite(UF, counter, tmp.predIdx, tmp.endIdx, tmp.value, 1);
+            unite(nodes, counter, tmp.src, tmp.dest, tmp.cost, true);
         } else {
-            eCrossRiv.push(tmp);
+            river_edges_PQ1.push(tmp);
         }
     }
 
     // Kruscal's alg cross the river
     while (counter > N - B) {
-        edge tmp = eCrossRiv.top();
-        eCrossRiv.pop();
+        Edge tmp = river_edges_PQ1.top();
+        river_edges_PQ1.pop();
 
         // if i can add it
-        unite(UF, counter, tmp.predIdx, tmp.endIdx, tmp.value, 1);
+        unite(nodes, counter, tmp.src, tmp.dest, tmp.cost, true);
     }
 
     // Kruscal's alg to other
-    while (UF[find(UF, 0)].capacity != N) {
-        edge tmp = eOther.top();
-        eOther.pop();
+    while (nodes[find(nodes, 0)].capacity != N) {
+        Edge tmp = other_edges_PQ.top();
+        other_edges_PQ.pop();
 
         // if i can add it
-        unite(UF, counter, tmp.predIdx, tmp.endIdx, tmp.value, 0);
+        unite(nodes, counter, tmp.src, tmp.dest, tmp.cost, false);
     }
 
-    cout << UF[find(UF, 0)].val << endl;
+    cout << nodes[find(nodes, 0)].val << endl;
 }
 
 
