8b480b6becb39b8d1ab7cb0f4d2ed3dfb5b63cea
kimoki-boop <vojtechkuzdas@gmail.com>
2023-01-06 11:14:57 +0100

    surv-peaks: recursion not working

---
+++ 11_surv_peaks/surv_peaks.cpp
@@ -0,0 +1,227 @@
+//
+// Created by kuzdavoj@fel.cvut.cz on 1/6/2023.
+//
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <climits>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Path {
+    bool contains_CP;
+    vector<int> sequence;
+
+    bool operator<(const Path &rhs) const {
+        return sequence.size() < rhs.sequence.size();
+    }
+
+    Path(bool containsCp, const vector<int> &sequence) : contains_CP(containsCp), sequence(sequence) {}
+};
+
+
+vector<vector<int>> tarjan_scc_adj(vector<vector<int>> &adj, vector<int> &comp_of, int N) { // consider const
+    const int UNDEF = INT32_MAX;
+    vector<int> index(N, UNDEF);
+    vector<int> lowlink(N, UNDEF);
+    vector<bool> on_stack(N, false);
+    stack<int> stck;
+    vector<vector<int>> comps;
+    int comp_num = 0;
+    stack<pair<int, int>> call_stack; // recursion argument stack
+    vector<int> scc_of(N, UNDEF);
+    int i = 0;
+
+    for (int v = 0; v < N; ++v) {
+        if (index[v] != UNDEF) continue;
+        call_stack.push({v, 0});
+        while (!call_stack.empty()) {
+            v = call_stack.top().first;
+            int pi = call_stack.top().second;
+            call_stack.pop();
+            // uzel nenavstiven
+            if (pi == 0) {
+                index[v] = i;
+                lowlink[v] = i;
+                i++;
+                stck.push(v);
+                on_stack[v] = true;
+            }
+            // "recursed" on something
+            if (pi > 0) {
+                int prev = adj[v][pi - 1];
+                lowlink[v] = min(lowlink[v], lowlink[prev]);
+            }
+            // recurse on
+            while (pi < adj[v].size() && index[adj[v][pi]] != UNDEF) {
+                int next = adj[v][pi];
+                if (on_stack[next]) {
+                    lowlink[v] = min(lowlink[v], index[next]);
+                }
+                pi++;
+            }
+            // found vert without index
+            if (pi < adj[v].size()) {
+                int next = adj[v][pi];
+                call_stack.push({v, pi + 1});
+                call_stack.push({next, 0});
+                continue;
+            }
+            // vertex is root of SCC
+            if (lowlink[v] == index[v]) {
+                vector<int> scc;
+                while (true) {
+                    auto next = stck.top();
+                    stck.pop();
+                    on_stack[next] = false;
+                    scc.push_back(next);
+                    comp_of[next] = comp_num;
+                    if (next == v) {
+                        comp_num++;
+                        break;
+                    }
+                }
+                comps.push_back(scc);
+            }
+        }
+    }
+    return comps;
+}
+
+
+vector<set<int>> condense_graph(vector<vector<int>> adj,
+                    int components_size,
+                    vector<int> comp_of) {
+    // kondenzace komponent:
+    // pro kazdou komponentu vytvorim uzel v ADJ_CONDENSED
+    // pro kazdej uzel v ADJ
+            // 1) v jake je komponente
+            // 2) do kterych uzlu vedou jejich hrany (do jakych komponent)
+    vector<set<int>> adj_cond(components_size);
+    for (int src = 0; src < adj.size(); ++src) {
+        if(src == 9 || src == 14 || src == 10|| src == 15) {
+            cout << "bug";
+        }
+        int src_component = comp_of[src];
+        for (int d = 0; d < adj[src].size(); ++d) {
+            int dest = adj[src][d];
+            int dest_component = comp_of[dest];
+            if(dest_component != src_component) {
+                adj_cond[src_component].insert(dest_component);
+            }
+        }
+    }
+    return adj_cond;
+}
+
+vector<Path> PATHS; // TODO: muze to bejt PQ
+
+
+void recursiveDFS(int current, vector<set<int>>& adjacencyList, int CP_comp, Path curr_path, vector<bool>& visited) {
+    // Process the current node here
+    visited[current] = true;
+    curr_path.sequence.push_back(current);
+    if (current == CP_comp) {
+        curr_path.contains_CP = true;
+    }
+    // pokud bychom se vraceli
+    // nebo pokud jsme v komponente ktera nikam nevede
+    // zapisujeme path
+
+
+    bool curr_node_is_final = true;
+    for (int neighbor : adjacencyList[current]) {
+        if (!visited[neighbor]) {
+            curr_node_is_final = false;
+            recursiveDFS(neighbor, adjacencyList, CP_comp, curr_path, visited);
+        }
+    }
+    if (curr_node_is_final && curr_path.contains_CP) {
+        if(curr_path.sequence[curr_path.sequence.size()-1] == 3) {
+            cout << "bug";
+        }
+        PATHS.push_back(curr_path);
+    }
+}
+
+
+void ite_DFS(int start, vector<set<int>>& adjacencyList, int CP_comp, Path curr_path) {
+    stack<int> S;
+    vector<bool> visited(adjacencyList.size(), false);
+
+    S.push(start);
+    visited[start] = true;
+
+    while (!S.empty()) {
+        int current = S.top();
+        S.pop();
+
+
+        // Process the current node here
+
+        for (int neighbor : adjacencyList[current]) {
+            if (!visited[neighbor]) {
+                S.push(neighbor);
+                visited[neighbor] = true;
+            }
+        }
+    }
+}
+
+
+int main() {
+    int N; // # bodu v parku
+    int M; // # tracku v parku
+    int C; // # oznaceni peaku
+
+    cin >> N >> M >> C;
+    vector<vector<int>> adj(N);
+
+    for (int i = 0; i < M; ++i) {
+        int src, dest;
+        cin >> src >> dest;
+        adj[src].push_back(dest);
+    }
+
+    vector<int> comp_of(N, INT32_MAX);
+    vector<vector<int>> components = tarjan_scc_adj(adj, comp_of, N);
+
+    vector<set<int>> adj_cond = condense_graph(adj, (int)components.size(), comp_of);
+    // dfs ze vsech do vsech
+    // v moment kdy jsem v end nodu
+    // eviduju sekvenci a zda obsahuje CP
+    int CP_comp = comp_of[C];
+
+    for (int i = 0; i < adj_cond.size(); ++i) {
+        Path curr_path{false, {}};
+        vector<bool> visited(adj_cond.size(), false);
+        /// recursive DFS uklada cesty do PATHS
+        if(i == 10) {
+            cout << "bug";
+        }
+        recursiveDFS(i, adj_cond, CP_comp, curr_path, visited);
+    }
+    // najdi nejdelsi pathsy ktere obsahuji CP
+
+    sort(PATHS.begin(), PATHS.end());
+
+
+    cout << "done";
+
+}
+
+
+
+
+
+
+
+
+
+
+
