01caff93a13cf5ee4531b34a3e32f84420ba0fde
kimoki-boop <vojtechkuzdas@gmail.com>
2023-01-23 13:22:17 +0100

    new_hello dont rfct too much

--- 13_3rd_zk/hello.cpp
+++ 13_3rd_zk/hello.cpp
@@ -20,74 +20,77 @@ using ci = const int;
 
 
 
-int F, Mmax, Xo, x, G;
+int F, Mmax, x0, x;
 vector<int> PRIMES;
+int COUNT = 0;
 
 
 
-vector<int> eratosthenes(int mmax){
+vector<int> sieve(int mmax){
     vector<int> res;
     vector<bool> primes_erat(mmax, true);
     primes_erat[0] = false;
     primes_erat[1] = false;
     for (int prime = 2; prime < primes_erat.size(); prime++) {
-        if (primes_erat[prime]) {
-            res.push_back(prime);
-            for (int i = prime * prime; i < primes_erat.size(); i += prime) {
-                primes_erat[i] = false;
-            }
+        if (!primes_erat[prime]) continue;
+        res.push_back(prime);
+        for (int i = prime * prime; i < primes_erat.size(); i += prime) {
+            primes_erat[i] = false;
         }
     }
     return res;
 }
 
-unsigned long modulo_multiplication(cul& a, cul& b, cul& m) {
-    unsigned long a_hi = a >> 24, a_lo = a & ((1 << 24) - 1);
-    unsigned long b_hi = b >> 24, b_lo = b & ((1 << 24) - 1);
-    unsigned long result = ((((a_hi*b_hi << 16) % m) << 16) % m) << 16;
-    result += ((a_lo*b_hi+a_hi*b_lo) << 24) + a_lo*b_lo;
-    return result % m;
+
+
+
+cll get_A(cll& combs_product, cll& M) {
+    if (M % 4 != 0) {
+        return combs_product + 1;
+    } else {
+        return combs_product * 2 + 1;
+    }
 }
 
-
-
-
-void find_M(ci& size, const vector<int>& combs, cll& combs_product,ci c_prev, cll& acc){
-    for (int i = size; i < combs.size(); ++i) {
-        if (acc * combs[i] <= Mmax){
-            cll& M = acc * combs[i];
-            cll& A = (M % 4 == 0) ? combs_product*2 + 1 : combs_product + 1;
+void search_modulo(ci& size, const vector<int>& combinations, cll& combs_product,ci c_prev, cll& pfp){
+    for (int i = size; i < combinations.size(); ++i) {
+        if (pfp * combinations[i] > Mmax) {
+            break;
+        } else {
+            cll &M = pfp * combinations[i];
+            cll &A = get_A(combs_product, M);
             auto C_prev = c_prev;
-            if (A < M){
-                if (__gcd(A,M)== 1) {
-                    long long c = (x - A*Xo) % M;
+            if (A >= M) {}
+            else {
+                if (__gcd(A, M) != 1) {}
+                else {
+                    long long c = (x - A * x0) % M;
                     c = c < 0 ? c + M : c;
-                    if (__gcd(c, M) == 1 && (A*Xo+c)%M == x) {
-                        G += 1;
+                    if (__gcd(c, M) != 1 || (A * x0 + c) % M != x) {}
+                    else {
+                        COUNT += 1;
                         C_prev = c;
                     }
                 }
             }
-            find_M(i, combs, combs_product, C_prev, M);
-        }else{
-            break;
+            search_modulo(i, combinations, combs_product, C_prev, M);
         }
     }
 }
 
 
-void find_combs(ci& index, ci& size, vector<int>& combs, cll& acc, ci& f){
-    if (size == f){
-        find_M(0, combs,acc,0, acc);
-    }else{
+void search_the_combinations(ci& index, ci& size, vector<int>& combs, cll& acc, ci& f){
+    if (size != f) {
         for (int i = index; i < PRIMES.size(); ++i) {
             combs[size] = PRIMES[i];
-            if (acc*PRIMES[i] <= Mmax){
-                find_combs(i + 1, size + 1, combs,acc*PRIMES[i], f);
-            }else{
+            if (acc * PRIMES[i] > Mmax) {
                 break;
+            } else {
+                search_the_combinations(i + 1, size + 1, combs, acc * PRIMES[i], f);
             }
         }
+    } else {
+        search_modulo(0, combs, acc, 0, acc);
     }
 
 }
@@ -96,10 +99,9 @@ void find_combs(ci& index, ci& size, vector<int>& combs, cll& acc, ci& f){
 
 
 int main() {
-    cin >> F >> Mmax >> Xo >> x;
-    G = 0;
-    PRIMES = eratosthenes(Mmax);
+    cin >> F >> Mmax >> x0 >> x;
+    PRIMES = sieve(Mmax);
     vector<int> combs(F);
-    find_combs(0, 0, combs,1, F);
-    cout << G << endl;
+    search_the_combinations(0, 0, combs,1, F);
+    cout << COUNT << endl;
 }
