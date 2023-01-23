b5f29da310a381c996cadab6ff6675ea20ba8aa5
kimoki-boop <vojtechkuzdas@gmail.com>
2023-01-23 12:51:46 +0100

    rfctr 8

--- 13_3rd_zk/hello.cpp
+++ 13_3rd_zk/hello.cpp
@@ -45,34 +45,34 @@ bool prod_prod_lesser(const lli &acc, const vector<uint> &vector, uint& mmax, ui
 void find_M(const uint& size, const vector<uint>& combs, const ll& pf_prod, const int c_prev, const ll& acc){
     for (uint i = size; i < combs.size(); ++i) {
 
-        if (prod_prod_lesser(acc, combs, Mmax, i)){
+        if (!prod_prod_lesser(acc, combs, Mmax, i)) {
+            break;
+        } else {
 
             ll M = acc * combs[i];
             ll A = pf_prod + 1;
-            if(M % 4 == 0) A = pf_prod * 2 + 1;
+            if (M % 4 == 0) A = pf_prod * 2 + 1;
 
             int C_prev = c_prev;
-            if (am_lesser(A, M)){
+            if (am_lesser(A, M)) {
                 bool AM_coprime = __gcd(A, M) == 1;
                 if (!AM_coprime) continue;
-                ll c = (x - A*Xo) % M;
+                ll c = (x - A * Xo) % M;
                 c = c < 0 ? c + M : c;
                 bool cm_coprime = __gcd(c, M) == 1;
-                bool eq_x = (A*Xo+c)%M == x;
-                if (cm_coprime  && eq_x) {
+                bool eq_x = (A * Xo + c) % M == x;
+                if (cm_coprime && eq_x) {
                     COUNT += 1;
                     C_prev = c;
                 }
             }
             find_M(i, combs, pf_prod, C_prev, M);
-        }else{
-            break;
         }
     }
 }
 
 
-void find_combs(uint& size, vector<uint>& combinations, uint& index, ll& pf_prod, const uint& f){
+void search_combinations(uint& size, vector<uint>& combinations, uint& index, ll& pf_prod, const uint& f){
 
     /// pokud NEMAME delku stejnou jako prvocisla
     if (size != f){
@@ -84,7 +84,7 @@ void find_combs(uint& size, vector<uint>& combinations, uint& index, ll& pf_prod
                 uint idx = i + 1;
                 uint sz = size + 1;
                 ll pp = PRIMES[i] * pf_prod;
-                find_combs(sz, combinations, idx, pp, f);
+                search_combinations(sz, combinations, idx, pp, f);
             }
         }
     }
@@ -109,6 +109,6 @@ int main() {
     vector<uint> combs(F);
 //    int& size, vector<uint>& combs, int& index, ll& pf_prod, const uint& f
     ll pf_prod = 1;
-    find_combs(FIRST_SIZE, combs, FIRST_INDEX,  pf_prod, F);
+    search_combinations(FIRST_SIZE, combs, FIRST_INDEX, pf_prod, F);
     cout << COUNT << endl;
 }
