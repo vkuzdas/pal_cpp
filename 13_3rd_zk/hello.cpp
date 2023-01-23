357a41f832b3f5a192a7122a084bb33c57289e0d
kimoki-boop <vojtechkuzdas@gmail.com>
2023-01-23 13:15:12 +0100

    new_hello dont rfct too much

--- 13_3rd_zk/hello.cpp
+++ 13_3rd_zk/hello.cpp
@@ -1,121 +1,105 @@
 #include <iostream>
 #include <vector>
+#include<iostream>
+#include<cmath>
+#include <regex>
 #include <algorithm>
+
+using namespace std;
 
 using namespace std;
 using uint = unsigned int;
 using ll = long long;
 using lli = long long int;
 
-
-uint F, Mmax, Xo, x;
-uint COUNT = 0;
-vector<uint> PRIMES;
-
-uint FIRST_INDEX = 0;
-uint FIRST_SIZE = 0;
-ll PF_PROD = 1;
+using cul = const unsigned long;
+using cll = const long long;
+using ci = const int;
 
 
-vector<uint> sieve(uint mmax);
-bool am_lesser(lli A, const lli M);
-bool prod_prod_lesser(const lli &acc, const vector<uint> &vector, uint& mmax, uint& i);
-void get_M(const uint& size, const vector<uint>& combs, const ll& pf_prod, const int c_prev, const ll& pfp);
-void search_combinations(uint& size, vector<uint>& combinations, uint& index, ll& pf_prod, const uint& f);
 
+
+
+int F, Mmax, Xo, x, G;
+vector<int> PRIMES;
+
+
+
+vector<int> eratosthenes(int mmax){
+    vector<int> res;
+    vector<bool> primes_erat(mmax, true);
+    primes_erat[0] = false;
+    primes_erat[1] = false;
+    for (int prime = 2; prime < primes_erat.size(); prime++) {
+        if (primes_erat[prime]) {
+            res.push_back(prime);
+            for (int i = prime * prime; i < primes_erat.size(); i += prime) {
+                primes_erat[i] = false;
+            }
+        }
+    }
+    return res;
+}
+
+unsigned long modulo_multiplication(cul& a, cul& b, cul& m) {
+    unsigned long a_hi = a >> 24, a_lo = a & ((1 << 24) - 1);
+    unsigned long b_hi = b >> 24, b_lo = b & ((1 << 24) - 1);
+    unsigned long result = ((((a_hi*b_hi << 16) % m) << 16) % m) << 16;
+    result += ((a_lo*b_hi+a_hi*b_lo) << 24) + a_lo*b_lo;
+    return result % m;
+}
+
+
+
+
+void find_M(ci& size, const vector<int>& combs, cll& combs_product,ci c_prev, cll& acc){
+    for (int i = size; i < combs.size(); ++i) {
+        if (acc * combs[i] <= Mmax){
+            cll& M = acc * combs[i];
+            cll& A = (M % 4 == 0) ? combs_product*2 + 1 : combs_product + 1;
+            auto C_prev = c_prev;
+            if (A < M){
+                if (__gcd(A,M)== 1) {
+                    long long c = (x - A*Xo) % M;
+                    c = c < 0 ? c + M : c;
+                    if (__gcd(c, M) == 1 && (A*Xo+c)%M == x) {
+                        G += 1;
+                        C_prev = c;
+                    }
+                }
+            }
+            find_M(i, combs, combs_product, C_prev, M);
+        }else{
+            break;
+        }
+    }
+}
+
+
+void find_combs(ci& index, ci& size, vector<int>& combs, cll& acc, ci& f){
+    if (size == f){
+        find_M(0, combs,acc,0, acc);
+    }else{
+        for (int i = index; i < PRIMES.size(); ++i) {
+            combs[size] = PRIMES[i];
+            if (acc*PRIMES[i] <= Mmax){
+                find_combs(i + 1, size + 1, combs,acc*PRIMES[i], f);
+            }else{
+                break;
+            }
+        }
+    }
+
+}
 
 
 
 
 int main() {
-
-
-
     cin >> F >> Mmax >> Xo >> x;
-    PRIMES = sieve(Mmax);
-
-    vector<uint> combs(F);
-//    int& size, vector<uint>& combs, int& index, ll& pf_prod, const uint& f
-    search_combinations(FIRST_SIZE, combs, FIRST_INDEX, PF_PROD, F);
-    cout << COUNT << endl;
-}
-
-
-bool am_lesser(lli A, const lli M) {
-    return A < M;
-}
-
-bool prod_prod_lesser(const lli &acc, const vector<uint> &vector, uint& mmax, uint& i) {
-    return acc * vector[i] <= Mmax;
-}
-
-void get_M(const uint& size, const vector<uint>& combs, const ll& pf_prod, const uint& c_prev, const ll& pfp){
-    for (uint i = size; i < combs.size(); ++i) {
-
-        if (!prod_prod_lesser(pfp, combs, Mmax, i)) {
-            break;
-        } else {
-
-            ll M = pfp * combs[i];
-            ll A = pf_prod + 1;
-            if (M % 4 == 0) A = pf_prod * 2 + 1;
-
-            uint C_prev = c_prev;
-            if (am_lesser(A, M)) {
-                bool AM_coprime = (1 == __gcd(A, M));
-                if (!AM_coprime) continue;
-                ll c = (x - A * Xo) % M;
-                c = c < 0 ? c + M : c;
-                bool cm_coprime = (1 == __gcd(c, M));
-                bool eq_x = (A * Xo + c) % M == x;
-                if (cm_coprime && eq_x) {
-                    COUNT += 1;
-                    C_prev = c;
-                }
-            }
-            get_M(i, combs, pf_prod, C_prev, M);
-        }
-    }
-}
-
-
-void search_combinations(uint& size, vector<uint>& combinations, uint& index, ll& pf_prod, const uint& f){
-
-    /// pokud NEMAME delku stejnou jako prvocisla
-    if (size != f){
-        for (uint i = index; i < PRIMES.size(); ++i) {
-            combinations[size] = PRIMES[i];
-            if (pf_prod * PRIMES[i] > Mmax) {
-                break;
-            } else if (pf_prod * PRIMES[i] <= Mmax) {
-                uint idx = i + 1;
-                uint sz = size + 1;
-                ll pp = PRIMES[i] * pf_prod;
-                search_combinations(sz, combinations, idx, pp, f);
-            }
-        }
-    }
-
-        /// pokud MAME delku stejnou jako prvocisla
-    else if (size == f){
-        get_M(FIRST_SIZE, combinations, pf_prod, FIRST_INDEX, pf_prod);
-    }
-
-}
-
-
-vector<uint> sieve(uint mmax){
-    vector<uint> result;
-    vector<bool> p_bool(mmax, true);
-    p_bool[0] = false;
-    p_bool[1] = false;
-    for (int p = 2; p < p_bool.size(); p++) {
-        if (p_bool[p]) {
-            result.push_back(p);
-            for (int i = p*p; i < p_bool.size(); i = i + p) {
-                p_bool[i] = false;
-            }
-        }
-    }
-    return result;
+    G = 0;
+    PRIMES = eratosthenes(Mmax);
+    vector<int> combs(F);
+    find_combs(0, 0, combs,1, F);
+    cout << G << endl;
 }
